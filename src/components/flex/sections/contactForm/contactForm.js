4287c15a0d7f775e940145a71e3c7cce9a9e1a0c
dieudo25 <twahirwa25@gmail.com>
2022-11-20 00:56:16 +0100

    final email notifications update

--- src/components/flex/sections/contactForm/contactForm.js
+++ src/components/flex/sections/contactForm/contactForm.js
@@ -1,4 +1,4 @@
-import React, { useState, createContext } from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 import { BsCheckCircle } from "react-icons/bs"
 
