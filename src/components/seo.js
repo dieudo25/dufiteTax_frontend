8e783c0ed5622a5c041b1ccbc22b5ad11a46b6da
dieudo25 <twahirwa25@gmail.com>
2022-12-04 13:07:51 +0100

    add google analutics

--- src/components/seo.js
+++ src/components/seo.js
@@ -62,7 +62,15 @@ function Seo({ title, description, lang, meta, keywords }) {
           content: process.env.GOOGLE_SITE_VERIFICATION,
         }
       ].concat(meta)}
-    >
+    >      
+      {/* <!-- Google tag (gtag.js) --> */}
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-EWHZ8QTV1C"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)};
+        gtag('js', new Date());
+        gtag('config', 'G-EWHZ8QTV1C');
+      </script>
     </Helmet>
   )
 }
