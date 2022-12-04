ff78e83ad8a47c85a27dd2ba1e1bfc44ecc20be2
dieudo25 <twahirwa25@gmail.com>
2022-12-04 15:24:20 +0100

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,5 +87,30 @@ module.exports = {
         environments: ['production', 'development']
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-250446471-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "dufitetaxadvisors.be",
+        // defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
   ],
 };
